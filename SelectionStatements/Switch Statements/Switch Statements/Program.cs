f001533a2bffc194e73e17781092579f7f984727
Thomas Marble <tmarble3@gmail.com>
2022-03-20 12:35:16 -0600

    switch/case

---
+++ SelectionStatements/Switch Statements/Switch Statements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Switch_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject?");
+            var response = Console.ReadLine(); 
+
+            switch (response)
+            {
+                case "Math":
+                    Console.WriteLine("I teach Math");
+                    break;
+
+                case "English":
+                    Console.WriteLine("I did not like English class.");
+                    break;
+                case "science":
+                    Console.WriteLine("I like Science too");
+                    break ;
+                case "History":
+                    Console.WriteLine("I fell asleep in history");
+                    break;
+             
+                default:
+                    Console.WriteLine("I have not heard of that");
+                    break ;
+
+
+               
+             
+            }
+            
+        }
+    }
+}
