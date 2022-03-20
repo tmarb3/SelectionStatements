6eaf27fb4d88cec035d26f136df0fb7a69677ef3
Thomas Marble <tmarble3@gmail.com>
2022-03-20 10:49:28 -0600

    initial commit

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,7 +6,31 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("See if you can guess the number!");
+
+            var random = new Random();
+            var number = random.Next(1, 1000);
+
+            Console.WriteLine("Input your guess:");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if(guess > number)
+            {
+                Console.WriteLine("too high!");
+            }
+
+            else if(guess < number)
+            {
+                Console.WriteLine("too low!");
+            }
+
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+
         }
     }
 }
